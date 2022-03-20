7d95bffc172933f33a1a9ca7224b3f48730c8a62
princerajputana6 <princerajputana6@gmail.com>
2022-03-20 13:58:31 +0530

    .

--- api/index.js
+++ api/index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use("/images", express.static(path.join(__dirname, "/images")));
 
 mongoose
-  .connect(process.env.MONGO_URL, {
+  .connect("mongodb+srv://abcd:abcd@cluster0.cwhsn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
